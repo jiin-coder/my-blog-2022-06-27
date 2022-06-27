3581cdad13f4f937926a2953cebc2d740f4beabe
kimjiae <fpahstiq123@naver.com>
2022-06-28 03:33:33 +0900

    세팅 (Initial commit from gatsby)

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `My Blog 2022 06 27`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-transformer-remark", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
